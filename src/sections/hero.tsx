bef6ee5996499e12bb7d892cf8c7824410e6a722
Design-With-Tanmay <design.with.tanmay@gmail.com>
2026-01-16 16:19:01 +0530

    mobile fix

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,61 +1,34 @@
 import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import heroBg from '~/assets/hero-bg.svg?raw';
-import lights from '~/assets/lights.svg?raw';
+import heroDashboard from '~/assets/hero-dashboard.svg';
+import heroMobile from '~/assets/hero-mobile.svg';
 
 export const Hero = component$(() => {
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(async () => {
     const { gsap } = await import('gsap');
 
-    const group = document.querySelector<SVGGElement>('#randomDots');
-    if (!group) return;
+    const arrow = document.querySelector<SVGGElement>('#move');
+    if (!arrow) return;
 
-    const dots = Array.from(group.querySelectorAll<SVGCircleElement>('circle'));
-
-    const BASE_COLOR = '#18181b'; // initial color
-    const HIGHLIGHT_COLOR = '#3f3f46'; // red
-    const BATCH_SIZE = 120; // 12–30 sweet spot
-    const HIGHLIGHT_DURATION = 0.8; // seconds
-
-    // Ensure all dots start with base color
-    dots.forEach((dot) => dot.setAttribute('fill', BASE_COLOR));
-
-    function highlightRandomDots() {
-      const batch = gsap.utils.shuffle(dots).slice(0, BATCH_SIZE);
-
-      // Turn selected dots red
-      batch.forEach((dot) => {
-        gsap.to(dot, {
-          fill: HIGHLIGHT_COLOR,
-          duration: 0.2,
-          overwrite: 'auto',
-        });
-      });
-
-      // Revert them back after 1s
-      gsap.delayedCall(HIGHLIGHT_DURATION, () => {
-        batch.forEach((dot) => {
-          gsap.to(dot, {
-            fill: BASE_COLOR,
-            duration: 0.2,
-            overwrite: 'auto',
-          });
-        });
-
-        // Start next cycle AFTER revert
-        gsap.delayedCall(0.1, highlightRandomDots);
-      });
-    }
-
-    highlightRandomDots();
+    gsap.fromTo(
+      arrow,
+      { y: 740 },
+      {
+        y: -240,
+        duration: 6,
+        ease: 'none',
+        repeat: -1,
+      }
+    );
   });
 
   return (
-    <section class="relative">
-      <div class="flex justify-center overflow-hidden">
+    <section class="relative mx-auto">
+      <div class="flex justify-center overflow-hidden mx-auto">
         <div dangerouslySetInnerHTML={heroBg}></div>
       </div>
-      <div class="max-w-xl text-center px-4 absolute top-56 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div class="text-center w-full sm:max-w-lg  px-4 absolute top-56 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <h1 class="text-4xl text-center font-normal">
           One workspace to track every project, task and team
         </h1>
@@ -69,6 +42,18 @@ export const Hero = component$(() => {
         >
           Create Your Workspace
         </a>
+      </div>
+      <div class="mx-auto -mt-20 overflow-hidden px-4 lg:px-0">
+        <picture>
+          <source srcset={heroMobile} media="(max-width: 639px)" />
+          <img
+            src={heroDashboard}
+            alt="dashboard"
+            height={576}
+            width={1024}
+            class="mx-auto"
+          />
+        </picture>
       </div>
     </section>
   );
